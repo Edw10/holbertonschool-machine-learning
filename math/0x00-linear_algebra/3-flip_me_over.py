bd484f38b3ffecebcababe9a3a01e4bdb60ffde0
Edw10 <edward-1094@hotmail.com>
2021-04-19 03:23:13 +0000

    add 3-flip_me

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -5,9 +5,11 @@
 def matrix_transpose(matrix):
     """ return the transpose of a matrix """
     a = []
+    for i in range(len(matrix[0])):
+        a.append([])
     for i in matrix:
         c = 0
         for y in i:
-        a[c].append(y)
-        c += 1
+            a[c].append(y)
+            c += 1
     return a

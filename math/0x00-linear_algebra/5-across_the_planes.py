ab1973b536b39814bbe825d8ac2343986e8ef4b8
Edw10 <edward-1094@hotmail.com>
2021-04-19 03:41:54 +0000

    add 5

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,7 +4,7 @@
 
 def add_matrices2D(mat1, mat2):
     """ return the add of two matrices """
-    a = [[],[]]
+    a = [[], []]
     if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
         for i in range(2):
             a[0].append(mat1[0][i]+mat2[0][i])

850927a5c0e2064542641ad6f51c61c6bb8338c6
Edw10 <edward-1094@hotmail.com>
2021-04-19 04:44:16 +0000

    add

---
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" that performs matrix multiplication: """
+
+
+def mat_mul(mat1, mat2):
+    """ that performs matrix multiplication: """
+    if len(mat1[0]) != len(mat2):
+        return None
+    new_mat = []
+    for i in range(len(mat1)):
+        aux = []
+        for j in range(len(mat2[0])):
+            y = 0
+            for x in range(len(mat1[0])):
+                y += mat1[i][x] * mat2[x][j]
+            aux.append(y)
+            new_mat.append(aux)
+    return new_mat

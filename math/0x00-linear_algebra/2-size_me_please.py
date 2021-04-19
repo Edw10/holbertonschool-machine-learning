d75934d00a1708fc142cbce22c46d510198be29f
Edw10 <edward-1094@hotmail.com>
2021-04-19 02:54:33 +0000

    add

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
-    # sadasd
+    """ return the shape of a matrix """
     a = []
     while type(matrix) is not int:
         a.append(len(matrix))

8e5a8b9963f7bba86e6aae27c5855b05eed2894f
Edw10 <edward-1094@hotmail.com>
2021-04-19 04:42:11 +0000

    add

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Function cat_matrices """
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """ concat two matrices """
+    new_mat = []
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        for row in mat1:
+            new_mat.append(list(row))
+        for row in mat2:
+            new_mat.append(list(row))
+    if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        for row, element in zip(mat1, mat2):
+            new_mat.append(list(row) + element)
+    return new_mat

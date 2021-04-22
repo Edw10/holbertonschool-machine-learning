59c71b050e54282eb0a6e6f8e7e8790a5c0c5fb4
Edw10 <edward-1094@hotmail.com>
2021-04-22 19:25:12 +0000

    add

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" derivate poli """
+
+
+def poly_derivative(poly):
+    """ poly pocket """
+    if len(poly) == 0:
+        return None
+    d = []
+    if len(poly) == 1:
+        return [0]
+    for i in range(1, len(poly)):
+        d.append(poly[i]*i)
+    return d

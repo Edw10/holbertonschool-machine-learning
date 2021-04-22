efd9d9bdb6b5f96b34a2da332860b50a782dcb79
Edw10 <edward-1094@hotmail.com>
2021-04-22 18:51:18 +0000

    add

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Sum Total Function """
+
+
+def summation_i_squared(n):
+    """ summation_i_squared(n) function """
+    if n >= 1:
+        return (n)(n + 1)(2n + 1) / 6
+    return None

2e74d2497e663507d7361c747338930d9a5ff989
Edw10 <edward-1094@hotmail.com>
2021-04-22 19:50:47 +0000

    add

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -6,11 +6,12 @@ def poly_derivative(poly):
     """ poly pocket """
     if len(poly) == 0 or type(poly) != list:
         return None
-    if all(isinstance(x, (int, float)) for x in poly):
-        d = []    
-        if len(poly) == 1:
-            return [0]
-        for i in range(1, len(poly)):
-            d.append(poly[i]*i)
-        return d
-    return None
+    for i in poly:
+        if type(i) != int and type(i) != float:
+            return None
+    d = []    
+    if len(poly) == 1:
+        return [0]
+    for i in range(1, len(poly)):
+        d.append(poly[i]*i)
+    return d

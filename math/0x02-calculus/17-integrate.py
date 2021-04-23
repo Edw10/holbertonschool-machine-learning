cfcadf6e9c312b890e6c941c6454e0acc8ad555e
Edw10 <edward-1094@hotmail.com>
2021-04-23 04:43:43 +0000

    dd

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -2,11 +2,11 @@
 """ integral poli """
 
 
-def poly_integral(poly):
+def poly_integral(poly, C=0):
     """ poly pocket """
     if type(poly) != list or len(poly) == 0:
         return None
-    d = [0]
+    d = [C]
     for i in range(0, len(poly)):
         if poly[i] % (i+1) == 0:
             d.append(poly[i]//(i+1))
